1810ad8226af219843b64b7a52c43ec60920e7f1
Mohammed Hassaan <115967953+Hassaan-100@users.noreply.github.com>
2024-03-30 13:35:27 +0530

    Update Checkout.jsx

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -117,7 +117,7 @@ export default function Checkout() {
         <h2>Success!</h2>
         <p>Your order was submitted successfully.</p>
         <p>
-          We will get back to you with more details via email within the next
+          We will get back to you with more details within the next
           few minutes.
         </p>
         <p className="modal-actions">
